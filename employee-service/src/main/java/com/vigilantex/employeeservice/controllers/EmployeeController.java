e5ec59b62a3816648e7fc159a15d3821344a0e79
Vigilante-Ops <amandixit99@outlook.com>
2022-12-11 18:51:47 +0530

    added retry mechanism

--- employee-service/src/main/java/com/vigilantex/employeeservice/controllers/EmployeeController.java
+++ employee-service/src/main/java/com/vigilantex/employeeservice/controllers/EmployeeController.java
@@ -6,6 +6,7 @@ import com.vigilantex.employeeservice.entity.Employee;
 import com.vigilantex.employeeservice.repository.EmployeeRepo;
 import com.vigilantex.employeeservice.services.APIClient;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,11 +30,12 @@ public class EmployeeController
     {
           return new ResponseEntity<>(employeeRepo.save(employee), HttpStatus.CREATED);
     }
-
+// @CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
     @GetMapping("getEmployee")
-    @CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+    @Retry(fallbackMethod = "getDefaultDepartment",name = "${spring.application.name}")
     public ResponseEntity<employeeDTO> getEmployee(@RequestBody Employee employee)
     {
+        System.out.println("inside get employee method");
        Department department= apiClient.getDepartment(employee.getDepartmentCode());
        Employee employeefetch=employeeRepo.findByEmailId(employee.getEmailId());
        employeeDTO e=new employeeDTO();
@@ -51,6 +53,7 @@ public class EmployeeController
     }
     public ResponseEntity<employeeDTO> getDefaultDepartment(@RequestBody Employee employee,Throwable th)
     {
+        System.out.println("inside get fallback method");
 
         Employee employeefetch=employeeRepo.findByEmailId(employee.getEmailId());
         employeeDTO e=new employeeDTO();
